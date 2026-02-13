34242c725580a6610a2f26d7187cabbd3a660e44
gettingfunkier <dev.gettingfunkier@gmail.com>
2026-02-13 11:16:13 +0000

    fixed field issues

--- rango/models.py
+++ rango/models.py
@@ -15,6 +15,7 @@ class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
+    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
